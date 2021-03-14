941357f84027268680555741937a3c0a8a585887
AlanLee <w1299495070@outlook.com>
2021-03-15 03:14:47 +0800

    backup

---
+++ random_and_mode.py
@@ -0,0 +1,31 @@
+"""
+Try to "classify" samples based on random chance and always guessing
+the most popular category.
+尝试基于随机机会和总是猜测来“分类”样本
+最受欢迎的类别。
+"""
+import random
+from data import DataSet
+
+most_pop = 'TennisSwing'
+
+data = DataSet()
+nb_classes = len(data.classes)
+
+# Try a random guess.
+nb_random_matched = 0
+nb_mode_matched = 0
+for item in data.data:
+    choice = random.choice(data.classes)
+    actual = item[1]
+
+    if choice == actual:
+        nb_random_matched += 1
+
+    if actual == most_pop:
+        nb_mode_matched += 1
+
+random_accuracy = nb_random_matched / len(data.data)
+mode_accuracy = nb_mode_matched / len(data.data)
+print("Randomly matched %.2f%%" % (random_accuracy * 100))
+print("Mode matched %.2f%%" % (mode_accuracy * 100))
